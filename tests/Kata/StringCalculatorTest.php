1cb056f880d363dfaf858e92c648528f8505b994
Thibaut Cantet <thibaut.cantet@soat.fr>
2022-10-22 19:39:49 +0200

    with a pipe

--- tests/Kata/StringCalculatorTest.php
+++ tests/Kata/StringCalculatorTest.php
@@ -33,4 +33,17 @@ final class StringCalculatorTest extends TestCase
         $this->assertEquals(444, $result);
     }
 
+    public function test_add_should_sum_all_numbers_when_delimiter_is_defined_and_is_a_pipe()
+    {
+        // given
+        $input = "[|]1|5|8||98|fg|178|154";
+        $calculator = new StringCalculator();
+
+        // when
+        $result = $calculator->add($input);
+
+        // then
+        $this->assertEquals(444, $result);
+    }
+
 }
